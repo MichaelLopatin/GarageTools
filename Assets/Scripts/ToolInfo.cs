7f9bfc2fc7a942813ca5509660cad012376149f0
Michael <fion@ngs.ru>
2018-05-21 19:33:15 +0700

    SaveProgress

---
+++ Assets/Scripts/ToolInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolInfo : MonoBehaviour
+{
+    private int cellID = -1;
+
+    public int CellID
+    {
+        get
+        {
+            return cellID;
+        }
+        set
+        {
+            cellID = value;
+        }
+    }
+}
+
+
